42ad4cae553925d942c8f53a512af0e56280916e
slpnk1990 <dimastroi@tut.by>
2022-03-07 16:25:22 +0300

    lending

--- app/landinpage/landing/admin.py
+++ app/landinpage/landing/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class HomePageAdmin(admin.ModelAdmin):
+    list_display = ('id','key', 'value')
+    list_display_links = ('id','key', "value")
+    search_fields = ('key', "value")
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('id','key', 'value', 'link')
+    list_display_links = ('id','key', "value", 'link')
+    search_fields = ('key', "value", 'link')
+
+class AboutUSModelAdmin(admin.ModelAdmin):
+    list_display = ('id','key', 'value')
+    list_display_links = ('id','key', "value")
+    search_fields = ('key', "value")
+
+class ContactSModelAdmin(admin.ModelAdmin):
+    list_display = ('id','key', 'value')
+    list_display_links = ('id','key', "value")
+    search_fields = ('key', "value")
+
+admin.site.register(ContactSModel, ContactSModelAdmin)
+admin.site.register(AboutUSModel, AboutUSModelAdmin )
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(HomePage, HomePageAdmin)
--- app/landinpage/landing/models.py
+++ app/landinpage/landing/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from distutils.command.upload import upload
+from tabnanny import verbose
 from django.db import models
 from django.utils.html import mark_safe
 
@@ -14,12 +15,22 @@ class HomePage(models.Model):
 
     def __str__(self):
         return '{}'.format(self.key)
+
+    class Meta:
+        verbose_name = 'Домашняя страница'
+        verbose_name_plural = 'Домашняя страница'
+        ordering = ['id']
         
 class Menu(models.Model):
     key = models.CharField(max_length=150, db_index=True, unique=True)
     value = models.TextField(blank=True, max_length=500, db_index=True)
     link = models.CharField(max_length=80, db_index=True, unique=True)
     date_pub = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+        ordering = ['id']
 
 class ImageAsset(models.Model):
     image = models.ImageField(upload_to='images')
@@ -47,6 +58,11 @@ class AboutUSModel(models.Model):
     def __str__(self):
         return '{}'.format(self.key)
 
+    class Meta:
+        verbose_name = 'О нас'
+        verbose_name_plural = 'О нас'
+        ordering = ['id']
+
 class ContactSModel(models.Model):
     key = models.CharField(max_length=150, db_index=True, unique=True)
     value = models.TextField(max_length=500, blank=True, db_index=True)
@@ -55,3 +71,8 @@ class ContactSModel(models.Model):
 
     def __str__(self):
         return '{}'.format(self.key)
+
+    class Meta:
+        verbose_name = 'Контакты'
+        verbose_name_plural = 'Контакты'
+        ordering = ['id']
